156ccdab229115aea487324acc38322b5bcfe16d
Austin Webb <stevenaustinwebb@gmail.com>
2019-09-23 13:47:32 -0500

    changed query

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -24,13 +24,28 @@ export default function Template({
 }
 
 export const pageQuery = graphql`
-  query($path: String!) {
-    markdownRemark(frontmatter: { path: { eq: $path } }) {
+  query BlogPostBySlug($slug: String!) {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
       html
       frontmatter {
-        date(formatString: "MMMM DD, YYYY")
-        path
         title
+        date(formatString: "MMMM DD, YYYY")
+        description
+        featuredImage {
+          childImageSharp {
+            sizes(maxWidth: 630, maxHeight: 700) {
+              ...GatsbyImageSharpSizes
+            }
+          }
+        }
       }
     }
   }
