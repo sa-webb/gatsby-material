b358f64680ec6b7d83b9f548d1c3a0cfc68ca747
Austin Webb <stevenaustinwebb@gmail.com>
2019-09-23 13:43:39 -0500

    old template

---
+++ src/temp/blogTemplate.js
@@ -0,0 +1,52 @@
+// import React from "react"
+// import { graphql } from "gatsby"
+// import Layout from "../layout/layout"
+
+// export default function Template({
+//   data, // this prop will be injected by the GraphQL query below.
+// }) {
+//   const { markdownRemark } = data // data.markdownRemark holds our post data
+//   const { frontmatter, html } = markdownRemark
+//   return (
+//     <Layout>
+//       <div className="blog-post-container">
+//         <div className="blog-post">
+//           <h1>{frontmatter.title}</h1>
+//           <h2>{frontmatter.date}</h2>
+//           <div
+//             className="blog-post-content"
+//             dangerouslySetInnerHTML={{ __html: html }}
+//           />
+//         </div>
+//       </div>
+//     </Layout>
+//   )
+// }
+
+// export const pageQuery = graphql`
+//   query BlogPostBySlug($slug: String!) {
+//     site {
+//       siteMetadata {
+//         title
+//         author
+//       }
+//     }
+//     markdownRemark(fields: { slug: { eq: $slug } }) {
+//       id
+//       excerpt(pruneLength: 160)
+//       html
+//       frontmatter {
+//         title
+//         date(formatString: "MMMM DD, YYYY")
+//         description
+//         featuredImage {
+//           childImageSharp {
+//             sizes(maxWidth: 630, maxHeight: 700) {
+//               ...GatsbyImageSharpSizes
+//             }
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
